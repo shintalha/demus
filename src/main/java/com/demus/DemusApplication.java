5154d4ecf18434c921373299efc3ac327c25e1f3
shintalha <sahintalha0@outlook.com>
2022-12-05 10:17:41 +0300

    Initial Spring Commit

---
+++ src/main/java/com/demus/DemusApplication.java
@@ -0,0 +1,13 @@
+package com.demus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demus/DemusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
